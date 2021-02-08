d4121292e985ed92ee53e730e52608d05fbbc42b
Anil Zeybek <anil.zeybek98@gmail.com>
2021-02-08 13:42:16 +0300

    add render after training complete

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ n_episodes = 10000
 if __name__ == "__main__":
     env = gym.make('LunarLander-v2')
     agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.n)
-    scores = deque(maxlen=100)
+    scores = deque(maxlen=10)
 
     for i in range(1, n_episodes+1):
         state = env.reset()
@@ -27,9 +27,20 @@ if __name__ == "__main__":
         agent.update_eps()
 
         mean_scores = np.mean(scores)
-        if mean_scores > 200:
+        if mean_scores > 250:
             print("Solved!")
+            break
 
         print(f'\rEpisode: {i}\tAverage Score: {mean_scores:.2f}', end="")
         if i % 10 == 0:
             print(f'\rEpisode: {i}\tAverage Score: {mean_scores:.2f}')
+
+    while True:
+        state = env.reset()
+        done = False
+        while not done:
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            
+            state = next_state
+            env.render()
